187c0629bec9959ed2de06e7d17c76d237551b67
ceres-datnl <dat.nguyenle@ceresolutions.com>
2022-10-21 09:11:05 +0700

    Update NewsController

---
+++ app/Http/Controllers/Backend/NewsController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Services\NewsService;
+use App\Http\Requests\Backend\News\AddNewsRequest;
+use App\Http\Requests\Backend\News\UpdateNewsRequest;
+
+class NewsController extends Controller
+{
+    protected $news;
+    protected $category;
+
+    public function __construct(NewsService $news)
+    {
+        $this->news = $news;
+    }
+
+    public function index()
+    {
+        return view('backend.news.index');
+    }
+
+    public function ajax(Request $request){
+//        dd($request->all());
+        $this->news->ajax($request);
+    }
+
+    public function add()
+    {
+        $dataCategory = $this->news->getCategoryData();
+        return view('backend.news.add')->with("dataCategory", $dataCategory);
+    }
+
+    public function store(AddNewsRequest $request)
+    {
+        try {
+            $this->news->insert($request);
+
+        } catch (\Exception $exception) {
+            return redirect('errors.404');
+        }
+
+        return redirect()->route('admin.news.index')->with(['success' => 'Add News Successful']);
+    }
+
+    public function view(Request $request)
+    {
+        try {
+            $dataNews = $this->news->find($request);
+        } catch (\Exception $exception) {
+            return redirect('errors/404');
+        }
+
+        return view('backend.news.view')->with("dataNews", $dataNews);
+    }
+
+    public function edit(Request $request)
+    {
+        try {
+            $dataCategory = $this->news->getCategoryData();
+            $dataNews = $this->news->find($request);
+
+            return view("backend.news.edit")
+                ->with('dataCategory', $dataCategory)
+                ->with('dataNews', $dataNews);
+        } catch (\Exception $exception) {
+            return redirect('errors/404');
+        }
+    }
+    public function update(UpdateNewsRequest $request){
+        try{
+            $this->news->update($request);
+            return redirect()->route('admin.news.index')->with(['success' => 'Update News Successful']);
+        } catch (\Exception $e){
+            return redirect('errors/404');
+        }
+    }
+    public function delete(Request $request){
+        $result = [
+            "status" => "OK",
+            "errors" => ""
+        ];
+        try {
+            $this->news->delete($request);
+        } catch (\Exception $exception) {
+            $result = [
+                "status" => "NO",
+                "errors" => "Fail"
+            ];
+        }
+        echo json_encode($result);
+        exit;
+    }
+    public function loadCategory(Request $request){
+        try {
+            $response = $this->news->loadCategory($request);
+        } catch (\Exception $exception){
+            $response = [
+                'errors' => true
+            ];
+        }
+        echo json_encode($response);
+        exit;
+    }
+}
